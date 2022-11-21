e726d581cbcabc71bcabed1695d9241bc7e57a6b
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2022-11-21 10:44:12 +0100

    Tast-1/5 completed

--- 01/app.js
+++ 01/app.js
@@ -7,11 +7,32 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+
     render() {
         console.log('render');
 
-        return <h1>{ this.state.counter }</h1>
+        return <h1>{this.state.counter}</h1>
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+        this.id = setInterval(() => {
+            const { counter } = this.state
+            this.setState({ counter: counter + 1 })
+        }, 1000)
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+        if (this.state.counter === 5) {
+            return this.componentWillUnmount()
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+        clearInterval(this.id)
     }
 }
 
-root.render(<App/>);
+root.render(<App />);
